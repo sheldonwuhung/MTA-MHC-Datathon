ba62f4754fe3aa419d23a666ad1f091bcb6b9d22
hedgehogz <sheldonwhung@gmail.com>
2025-09-22 18:16:00 -0400

    buttons

--- funcs.js
+++ funcs.js
@@ -7,19 +7,22 @@ const conclusionH2 = document.getElementById("conclusion");
 const nav = document.getElementById("nav");
 const navHeight = nav.getBoundingClientRect().height;
 
+const projectNameDiv = document.getElementById("project-name");
 const navButton1 = document.getElementById("nav-button-1");
 const navButton2 = document.getElementById("nav-button-2");
 const navButton3 = document.getElementById("nav-button-3");
 const navButton4 = document.getElementById("nav-button-4");
 const navButton5 = document.getElementById("nav-button-5");
 
-const navButtons = [navButton1, navButton2, navButton3, navButton4, navButton5];
-const windowLocations = [introductionH2, question1H2, question2H2, question3H2, conclusionH2];
+const navButtons = [projectNameDiv, navButton1, navButton2, navButton3, navButton4, navButton5];
+const windowLocations = [projectNameDiv, introductionH2, question1H2, question2H2, question3H2, conclusionH2];
 
 function ChangeWindowLocation (index) {
     const location = windowLocations[index].getBoundingClientRect();
-    const y = location.top + window.scrollY - navHeight*2;
+    let y = location.top + window.scrollY - navHeight*2;
 
+    if (index == 0) {y = 0};
+    console.log(y);
     window.scrollTo({ top: y, left: 0, behavior: 'smooth' });
 };
 
