aed1340235e0c8aba2d55d9bab3b33acfead0fc0
hedgehogz <sheldonwhung@gmail.com>
2025-09-22 17:57:06 -0400

    buttons

---
+++ funcs.js
@@ -0,0 +1,30 @@
+const introductionH2 = document.getElementById("introduction");
+const question1H2 = document.getElementById("question-1");
+const question2H2 = document.getElementById("question-2");
+const question3H2 = document.getElementById("question-3");
+const conclusionH2 = document.getElementById("conclusion");
+
+const nav = document.getElementById("nav");
+const navHeight = nav.getBoundingClientRect().height;
+
+const navButton1 = document.getElementById("nav-button-1");
+const navButton2 = document.getElementById("nav-button-2");
+const navButton3 = document.getElementById("nav-button-3");
+const navButton4 = document.getElementById("nav-button-4");
+const navButton5 = document.getElementById("nav-button-5");
+
+const navButtons = [navButton1, navButton2, navButton3, navButton4, navButton5];
+const windowLocations = [introductionH2, question1H2, question2H2, question3H2, conclusionH2];
+
+function ChangeWindowLocation (index) {
+    const location = windowLocations[index].getBoundingClientRect();
+    const y = location.top + window.scrollY - navHeight*2;
+
+    window.scrollTo({ top: y, left: 0, behavior: 'smooth' });
+};
+
+navButtons.forEach((button, index) => {
+    if (!button) return;
+    button.addEventListener('click', () => ChangeWindowLocation(index));
+});
+
